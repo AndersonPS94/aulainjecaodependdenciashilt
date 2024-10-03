9f29dda1a664b703d6adbd9809c92601ee8f9e63
AndersonPS <dson_adn@hotmail.com>
2024-10-03 11:44:13 -0300

    commit aula -> 16/26 prox aula, Projeto com Hilt

--- app/src/main/java/com/example/aulainjecaodependenciashilt/di/AppModulo.kt
+++ app/src/main/java/com/example/aulainjecaodependenciashilt/di/AppModulo.kt
@@ -12,12 +12,14 @@ import com.example.aulainjecaodependenciashilt.classes.Violao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.qualifiers.ActivityContext
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Named
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ActivityComponent::class)
 object AppModulo {
 
     @Provides
